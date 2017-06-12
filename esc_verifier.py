4c6cbc9189cf4fade74d54675056bf1eb8297e07
Jonathan Challinger <mr.challinger@gmail.com>
2017-06-12 11:40:44 -0700

    check forward and reverse

--- esc_verifier.py
+++ esc_verifier.py
@@ -2,10 +2,13 @@
 
 import serial
 import argparse
+from time import sleep
+from sys import exit
+
+DIR_RX = 0
+DIR_TX = 1
 
 parser = argparse.ArgumentParser()
-parser.add_argument('tx_csv', nargs=1)
-parser.add_argument('rx_csv', nargs=1)
 parser.add_argument('port', nargs=1)
 args = parser.parse_args()
 
@@ -13,28 +16,25 @@ def csv_text_to_struct(csv):
     ret = [(float(x[0]), int(x[1],0)) for x in [x.split(',') for x in csv.strip().split('\n')][1:]]
     return ret
 
-with open(args.tx_csv[0], 'r') as f:
-    tx_data = csv_text_to_struct(f.read())
+def validate_esc_settings(txfile, rxfile):
+    with open(txfile, 'r') as f:
+        tx_data = csv_text_to_struct(f.read())
 
-with open(args.rx_csv[0], 'r') as f:
-    rx_data = csv_text_to_struct(f.read())
+    with open(rxfile, 'r') as f:
+        rx_data = csv_text_to_struct(f.read())
 
-DIR_RX = 0
-DIR_TX = 1
+    tx_idx = 0
 
-tx_idx = 0
+    sequence = []
 
-sequence = []
+    for rx_tuple in rx_data:
+        if rx_tuple in tx_data:
+            direction = DIR_TX
+        else:
+            direction = DIR_RX
 
-for rx_tuple in rx_data:
-    if rx_tuple in tx_data:
-        direction = DIR_TX
-    else:
-        direction = DIR_RX
+        sequence.append((rx_tuple[1], direction))
 
-    sequence.append((rx_tuple[1], direction))
-
-def validate_esc_settings():
     try:
         ser = serial.Serial(args.port[0], 9600, timeout=1)
         for byte, direction in sequence:
@@ -51,9 +51,18 @@ def validate_esc_settings():
                 if rx_byte != chr(byte):
                     return False
     except:
-        print 'Exception'
         return False
 
     return True
 
-print "##### PASS! #####" if validate_esc_settings() else "##### FAIL! #####"
+sleep(1)
+print "Checking valid config 1"
+res_1 = validate_esc_settings("golden_tx.csv","golden_rx.csv")
+print "Config 1 PASS" if res_1 else "Config 1 FAIL"
+sleep(1)
+print "Checking valid config 2"
+res_2 = validate_esc_settings("golden_tx_rev.csv","golden_rx_rev.csv")
+print "Config 2 PASS" if res_2 else "Config 2 FAIL"
+
+print "##### PASS #####" if res_1 or res_2 else "##### FAIL #####"
+exit(0 if res_1 or res_2 else 1)
